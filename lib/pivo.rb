550014a7416533886be8bcf4397ab49125e71b15
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2015-01-10 21:38:12 +0900

    Implemented `pivo projects`  command

--- lib/pivo.rb
+++ lib/pivo.rb
@@ -1,5 +1,16 @@
 require "pivo/version"
 
+require 'yaml'
+require 'tracker_api'
+require 'thor'
+
 module Pivo
-  # Your code goes here...
+  class CLI < Thor
+    desc "projects", "listing project names"
+    def projects
+      token =  YAML.load(File.read("#{ENV['HOME']}/.pivo.yml"))['token']
+      client = TrackerApi::Client.new(token: token)
+      puts client.projects.map(&:name).join("\n")
+    end
+  end
 end
