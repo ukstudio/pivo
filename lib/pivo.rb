7cd62f827c9249a700894a2da6e9e2436fd48245
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2015-01-11 00:05:06 +0900

    Implemented `stories PROJECT_NAME` command

--- lib/pivo.rb
+++ lib/pivo.rb
@@ -8,9 +8,25 @@ module Pivo
   class CLI < Thor
     desc "projects", "listing project names"
     def projects
-      token =  YAML.load(File.read("#{ENV['HOME']}/.pivo.yml"))['token']
-      client = TrackerApi::Client.new(token: token)
       puts client.projects.map(&:name).join("\n")
+    end
+
+    desc "stories PROJECT_NAME", "listing project stories"
+    def stories(project_name)
+      project = client.projects.select {|project| project.name == project_name}[0]
+      project.stories.each do |story|
+        say "[#{story.current_state}] #{story.name}"
+      end
+    end
+
+    private
+
+    def client
+      TrackerApi::Client.new(token: token)
+    end
+
+    def token
+      YAML.load(File.read("#{ENV['HOME']}/.pivo.yml"))['token']
     end
   end
 end
